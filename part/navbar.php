7a0cddeba5eccec82c7eb300b7c69c5dc5482d77
vivcyber <mspinksun@gmail.com>
2022-05-29 20:07:16 +0800

    clean file

--- part/navbar.php
+++ part/navbar.php
@@ -86,34 +86,3 @@ if (!isset($pageName)) {
 
         
 
-
-
-
-
-<!-- <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light border-bottom border-primary">
-            <div class="container">
-              <a class="navbar-brand" href="index_.php">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link <?= $pageName == 'index' ? 'active' : '' ?>" href="index_.php">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link <?= $pageName == 'ab-list' ? 'active' : '' ?>" href="ad_list.php">ad-list</a>
-                  </li>
-                  <li class="nav-item">
-                   <a class="nav-link <?= $pageName == 'ab_add' ? 'active' : '' ?>" href="ad_add.php">新增通訊錄</a>
-                  </li>
-                 
-                  
-                </ul>
-               
-              </div>
-            </div>
-          </nav>
-
-    </div> -->
