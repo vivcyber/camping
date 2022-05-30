1a26838e917f5d2ee9ab6b90437790809c7dec4e
vivcyber <mspinksun@gmail.com>
2022-05-30 15:01:12 +0800

    改周邊景點

--- part/navbar.php
+++ part/navbar.php
@@ -41,7 +41,7 @@ if (!isset($pageName)) {
               <button type="button" class="btn btn-outline-light w-100 border-0">
               <a class="text-dark text-decoration-none<?= $pageName == 'ab-list' ? 'active' : '' ?>" href="campland_list.php">
               <i class="fa-solid fa-location-dot"></i>
-              園區導覽
+              周邊景點
               </a>
               </button>
               <button type="button" class="btn btn-outline-light w-100 border-0">
