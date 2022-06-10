7ad99ce48f75d03911710c8a4f915786d342b105
YenYu <diaoemily0830@gmail.com>
2022-06-10 15:53:14 +0800

    修改密碼api 新增購買紀錄

--- M_edit-password.php
+++ M_edit-password.php
@@ -7,7 +7,11 @@ $pageName = 'edit-password';
 $title = '會員卡';
 
 print_r($_SESSION);
-exit;
+if (!isset($_SESSION)) {
+    echo 'session empty!';
+    exit;
+}
+
 
 $password = isset($_SESSION['loginUser']['m_passwd']) ? $_SESSION['loginUser']['m_passwd'] : '';
 
@@ -25,35 +29,77 @@ if ($password == '') {
 
 <div class=" login-card" style="width:30rem;margin: 5rem auto ;">
     <div class="login-page-card-body">
-        <form method="post" onsubmit="return setPassword()">
+        <form name="form1" method="post" onsubmit="setPassword();return false;" novalidate>
             <div class="old-password d-flex">
                 <div class="member-title" style="font-style:normal;font-size:1rem;margin:0 10px;align-self:center">請輸入舊密碼:</div>
-                <input id="old-password" name="old-password" type="password" style="height: 25px;align-self:center">
-                <div class="caution-bar" style="text-shadow: none; font-style:normal;color:red;align-self:center;display:none;">密碼錯誤</div>
+                <input id="old-password" name="oldpassword" type="password" style="height: 25px;align-self:center">
+
             </div>
             <div class="new-password d-flex">
                 <div class="member-title" style="font-style:normal;font-size:1rem;margin:0 10px;align-self:center">請輸入新密碼:</div>
-                <input type="password" name="new-password" style="height: 25px;align-self:center">
+                <input type="password" name="newpassword" style="height: 25px;align-self:center">
             </div>
             <div class="new-password d-flex">
                 <div class="member-title" style="font-style:normal;font-size:1rem;margin:0 10px;align-self:center">請確認新密碼:</div>
-                <input type="password" style="height: 25px;align-self:center">
+                <input id="confirm" type="password" style="height: 25px;align-self:center">
+                <div class="caution-bar" style="text-shadow: none; font-style:normal;color:red;align-self:center;display:none;">密碼錯誤</div>
             </div>
-            <button class="login-button">提交</button>
+            <button type="submit" class="login-button">提交</button>
         </form>
     </div>
 </div>
 
 
+
+
 <?php include __DIR__ . '/c_part/c_scripts.php' ?>
 <script>
-    const oldP = document.querySelector('#old-password');
+    const oldP = document.querySelector('#oldpassword');
     const cBar = document.querySelector('.caution-bar');
     const password = '<?php echo $password ?>';
+    const newPassword = document.form1.newpassword;
+    const confirm = document.querySelector('#confirm');
+
+    // console.log(document.form1);
 
 
-    function setPassword() {
 
+    if (newPassword.value !== confirm.value) {
+
+        cBar.style.display = 'block';
+        cBar.innerText = '新密碼與確認密碼不符';
+
+    } else {
+
+        console.log('123');
+
+        async function setPassword() {
+
+            console.log('456');
+            // let isPass = true;
+
+            // if (!isPass) {
+            //     return; // 結束函式
+            // }
+
+            const fd = new FormData(document.form1);
+            const r = await fetch('M_edit-password-api.php', {
+                method: 'POST',
+                body: fd,
+            });
+
+            const result = await r.json();
+            console.log(result);
+
+
+            if (result.success) {
+
+                console.log('success');
+
+            } else {
+                console.log('error');
+            }
+        }
     }
 </script>
 <?php include __DIR__ . '/c_part/c_foot.php' ?>
